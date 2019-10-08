4e47ddd371bf0e153c8fc8ae329fca0576fb2be0
ianhunterharold <ianhunterharold@gmail.com>
2019-10-08 13:50:38 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function wakeDog(dogName,dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName,dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName,dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName,dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName,dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName,dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+
+
+let routine = [wakeDog,leashDog,throwFrisbee,walkHome,walkHome,unleashDog];
+
+function dailyRoutine(array){
+  for (let i =0; i <array.length; i++){
+    array[i]("Sunny","shep");
+  }
+}
+dailyRoutine(routine);
